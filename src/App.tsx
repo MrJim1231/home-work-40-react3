5b2337ad86e1f73eadfb411c1594384cb9107bfe
Oleh <berolegnik@gmail.com>
2025-09-20 13:02:46 +0300

    mobile

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,17 @@ function App() {
     <div className="container-fluid vh-100">
       <div className="row h-100">
         {/* Controlled Form */}
-        <div className="col-4 d-flex align-items-start justify-content-center p-3">
+        <div className="col-12 col-md-6 col-lg-4 d-flex align-items-start justify-content-center p-3">
           <ControlledForm />
         </div>
 
         {/* Uncontrolled Form */}
-        <div className="col-4 d-flex align-items-start justify-content-center p-3">
+        <div className="col-12 col-md-6 col-lg-4 d-flex align-items-start justify-content-center p-3">
           <UncontrolledForm />
         </div>
 
         {/* Users List */}
-        <div className="col-4 d-flex align-items-start justify-content-center p-3 overflow-auto">
+        <div className="col-12 col-md-12 col-lg-4 d-flex align-items-start justify-content-center p-3 overflow-auto">
           <StatefulUsers />
         </div>
       </div>
